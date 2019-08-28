f661aea6bd7a5f2174b73bb4289367d6f53e6052
Weber Calixto <webersouzacalixto@gmail.com>
2019-08-28 18:26:46 -0300

    Helloworld files

---
+++ include/global.hpp
@@ -0,0 +1,7 @@
+#ifndef GLOBAL_HPP
+#define GLOBAL_HPP
+#include <cstdio>
+#include <iostream>
+#include <thread>
+#include <string>
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "global.hpp"
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    std::cout << "HELLO WORLD" << std::endl;
+    return 0;
+}
