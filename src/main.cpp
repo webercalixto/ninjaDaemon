9265a7341090c19eab740c0507ec550b4d651112
Weber Calixto <webersouzacalixto@gmail.com>
2019-08-28 20:14:29 -0300

    Typo

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     ctx.logger->log("STARTING NINJADAEMON");
     if (argc < 2 || argv[1] == nullptr || !std::filesystem::exists(argv[1]))
     {
-        ctx.logger->log("No ini filename provided or ini file doest exists. exiting");
+        ctx.logger->log("No ini filename provided or ini file doesnt exist. exiting");
         return 0;
     }
     ctx.logger->log("STARTING NINJADAEMON");
