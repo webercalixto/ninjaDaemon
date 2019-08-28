6c33325626a46316df1519e51175baf0ce11407e
Weber Calixto <webersouzacalixto@gmail.com>
2019-08-28 18:54:01 -0300

    Class ninjaLogger

--- include/ninjaLogger.hpp
+++ include/ninjaLogger.hpp
@@ -1,5 +1,25 @@
 #ifndef NINJALOGGER_HPP
 #define NINJALOGGER_HPP
 
+class ninjaLogger
+{
+    private:
+    int logLevel;
+
+    public:
+    const int NOTICE_LVL;
+    const int DEBUG_LVL;
+    const int INFO_LVL;
+    const int WARNING_LVL;
+    const int ERR_LVL;
+    const int CRIT_LVL;
+    const int ALERT_LVL;
+    const int EMERG_LVL;
+    ninjaLogger();
+    ~ninjaLogger();
+    void setLogLevel(const int &level);
+    void log(const std::string &message) const;
+    void log(int level, const std::string &message) const;
+};
 
 #endif
--- src/ninjaLogger.cpp
+++ src/ninjaLogger.cpp
@@ -1,1 +1,39 @@
 #include "global.hpp"
+#include <syslog.h>
+
+ninjaLogger::ninjaLogger()
+    : NOTICE_LVL(LOG_NOTICE)
+    , DEBUG_LVL(LOG_DEBUG)
+    , INFO_LVL(LOG_INFO)
+    , WARNING_LVL(LOG_WARNING)
+    , ERR_LVL(LOG_ERR)
+    , CRIT_LVL(LOG_CRIT)
+    , ALERT_LVL(LOG_ALERT)
+    , EMERG_LVL(LOG_EMERG)
+{
+    setLogLevel(this->DEBUG_LVL);
+    openlog("|NINJADAEMON|", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+    this->log("ninjaDaemon: Log started");
+}
+
+ninjaLogger::~ninjaLogger()
+{
+    this->log("ninjaDaemon: ninjaLogger: Closing");
+    closelog();
+}
+
+void ninjaLogger::setLogLevel(const int &level)
+{
+    setlogmask(LOG_UPTO(level));
+    this->logLevel = level;
+}
+
+void ninjaLogger::log(const int level, const std::string &message) const
+{
+    syslog(level, "%s", message.c_str());
+    if (level <= this->CRIT_LVL) std::cout << message << std::endl;
+}
+void ninjaLogger::log(const std::string &message) const
+{
+    this->log(this->logLevel, message);
+}
