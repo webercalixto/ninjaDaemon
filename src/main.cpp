d5b795bc3e04c07c1a98c042bfbbbf164e840a8d
Weber Calixto <webersouzacalixto@gmail.com>
2019-08-28 20:09:55 -0300

    Checking for nullptr on ini filename

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ ninjaDaemon ctx;
 int main(int argc, char *argv[])
 {
     ctx.logger->log("STARTING NINJADAEMON");
-    if (argc < 2 || !std::filesystem::exists(argv[1]))
+    if (argc < 2 || argv[1] == nullptr || !std::filesystem::exists(argv[1]))
     {
         ctx.logger->log("No ini filename provided or ini file doest exists. exiting");
         return 0;
